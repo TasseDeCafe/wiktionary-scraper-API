a8f17048f84d417a9a738f09207dd769d82c95f7
sebastien <sebastien.stecker@gmail.com>
2020-02-23 14:29:42 +0100

    - removed password

--- conjugator.py
+++ conjugator.py
@@ -2,19 +2,7 @@ from language_functions import conjugate, decline_noun, decline_adjective
 from flask import Flask, request
 from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
-
-class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
-
-    def __str__(self):
-        return "User(id='%s')" % self.id
-
-users = [
-    User(1, 'sebastien', 'RDvNifgjtJ6p1DA')
-]
+from users_password import users
 
 username_table = {u.username: u for u in users}
 userid_table = {u.id: u for u in users}
